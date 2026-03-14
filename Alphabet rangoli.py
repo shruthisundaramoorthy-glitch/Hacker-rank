2da05c997b2eb63c5c19298f59ca5eabd1f6a678
Shruthisundaramoorthy <shruthisundaramoorthy@gmail.com>
2026-03-14 11:46:26 +0530

    Add files via upload

--- Alphabet rangoli.py
+++ Alphabet rangoli.py
@@ -2,12 +2,11 @@ def print_rangoli(size):
     import string
     alphabet = string.ascii_lowercase
 
-  
     lines = []
     for i in range(size):
         s = '-'.join(alphabet[size-1:i:-1] + alphabet[i:size])
         lines.append(s.center(4*size-3, '-'))
     
-   
+
     print('\n'.join(lines[:0:-1] + lines))
 
